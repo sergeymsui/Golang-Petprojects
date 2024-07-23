e4677bf940445c64824622d0748b17e157eefc0a
Sergey Malichenko <s.malichenko@yandex.ru>
2024-07-23 20:53:05 +0300

    feat: simple calculator release

--- 01-calculator/src/main.go
+++ 01-calculator/src/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+func calculate(line string) float64 {
+
+	for i, e := range line {
+
+		s := string(e)
+		v, _ := strconv.ParseFloat(line[:i], 64)
+		u, _ := strconv.ParseFloat(line[i+1:], 64)
+		if s == "+" {
+			return float64(v + u)
+		} else if s == "-" {
+			return float64(v - u)
+		} else if s == "*" {
+			return float64(v * u)
+		} else if s == "/" {
+			return float64(v / u)
+		}
+	}
+
+	return 0
+}
 
 func main() {
-	fmt.Println("Hello world")
+	var line string
+	fmt.Scan(&line)
+
+	fmt.Println(calculate((line)))
 }
